b16e70345b294af845a3b0e225e961c20e34df38
dokusasako <dokusasako@dokusasakonoiMac.local>
2020-07-09 18:14:26 +0900

    bind相当なものを実装するところまで

--- include/harmony.hpp
+++ include/harmony.hpp
@@ -2,8 +2,9 @@
 #include <utility>
 #include <ranges>
 
-namespace harmony::detail {
-  
+
+namespace harmony::inline concepts {
+
   template<typename T>
   using with_reference = T&;
   
@@ -16,19 +17,55 @@ namespace harmony::detail {
   concept weakly_indirectly_readable = requires(const T& t) {
     {*t} -> not_void;
   };
+}
 
+namespace harmony::detail {
+  
   struct unwrap_impl {
 
+    template<typename T>
+      requires std::is_pointer_v<T>
+    [[nodiscard]]
+    constexpr auto& operator()(T t) const noexcept {
+      return *t;
+    }
+
     template<weakly_indirectly_readable T>
-    constexpr auto& operator()(T& t) noexcept(noexcept(*t)) {
+      requires (not std::is_pointer_v<T>)
+    [[nodiscard]]
+    constexpr auto& operator()(T& t) const noexcept(noexcept(*t)) {
       return *t;
     }
 
     template<typename T>
       requires (not weakly_indirectly_readable<T>) and
-               requires(const T& t) { {t.value()} -> not_void; }
-    constexpr auto& operator()(T& t) noexcept(noexcept(*t)) {
-      return *t;
+               requires(T& t) { {t.value()} -> not_void; }
+    [[nodiscard]]
+    constexpr auto& operator()(T& t) const noexcept(noexcept(t.value())) {
+      return t.value();
+    }
+
+    template<std::ranges::range R>
+    [[nodiscard]]
+    constexpr auto operator()(R& t) const noexcept(noexcept(std::ranges::begin(t)) and noexcept(std::ranges::end(t))) {
+      struct wrap_view {
+        using iterator = std::ranges::iterator_t<R>;
+
+        iterator it;
+
+        [[no_unique_address]]
+        std::ranges::sentinel_t<R> se;
+
+        constexpr auto begin() const noexcept {
+          return it;
+        }
+
+        constexpr auto end() const noexcept {
+          return se;
+        }
+      };
+
+      return wrap_view{ .it = std::ranges::begin(t), .se = std::ranges::end(t)};
     }
   };
 
@@ -44,37 +81,39 @@ namespace harmony::inline concepts {
 
   template<typename T>
   concept unwrappable = requires(T& m) {
-    { harmony::cpo::unwrap(m) } -> detail::not_void;
+    { harmony::cpo::unwrap(m) } -> not_void;
   };
-}
-
-namespace harmony::detail {
 
   template<typename T>
   concept boolean_convertible = requires(const T& t) {
     bool(t);
   };
+}
+
+namespace harmony::detail {
 
   struct validate_impl {
 
     template<unwrappable T>
       requires boolean_convertible<T>
-    constexpr bool operator()(const T& t) noexcept(noexcept(bool(t))) {
+    [[nodiscard]]
+    constexpr bool operator()(const T& t) const noexcept(noexcept(bool(t))) {
       return bool(t);
     }
 
     template<unwrappable T>
       requires (not boolean_convertible<T>) and
                requires(const T& t) { {t.has_value()} -> std::same_as<bool>; }
-    constexpr bool operator()(const T& t) noexcept(noexcept(t.has_value())) {
+    [[nodiscard]]
+    constexpr bool operator()(const T& t) const noexcept(noexcept(t.has_value())) {
       return t.has_value();
     }
 
     template<unwrappable T>
-      requires requires(const T& t) {
-        {std::ranges::empty(t)} -> std::same_as<bool>;
-      }
-    constexpr bool operator()(const T& t) noexcept(noexcept(std::ranges::empty(t))) {
+      requires (not boolean_convertible<T>) and
+               requires(const T& t) { {std::ranges::empty(t)} -> std::same_as<bool>; }
+    [[nodiscard]]
+    constexpr bool operator()(const T& t) const noexcept(noexcept(std::ranges::empty(t))) {
       return not std::ranges::empty(t);
     }
   };
@@ -90,51 +129,130 @@ namespace harmony::inline cpo {
 namespace harmony::inline concepts {
 
   template<typename T>
-  concept maybe = requires(T& m) {
-    { harmony::cpo::validate(m) } -> std::same_as<bool>;
-  };
+  concept maybe =
+    unwrappable<T> and
+    requires(T& m) {
+      { harmony::cpo::validate(m) } -> std::same_as<bool>;
+    };
   
   template<typename T>
   concept list = maybe<T> and std::ranges::range<T>;
+
 }
 
 namespace harmony::traits {
+
+  template<typename T>
+  using unwrap_raw_t = decltype(harmony::cpo::unwrap(std::declval<T&>()));
   
   template<typename T>
-  using content_t = std::remove_cvref_t<decltype(harmony::cpo::unwrap(std::declval<T&>()))>;
+  using unwrap_t = std::remove_cvref_t<unwrap_raw_t<T>>;
+}
+
+namespace harmony::detail {
+
+  struct unit_impl {
+
+    template<unwrappable M, typename T>
+      requires std::assignable_from<traits::unwrap_raw_t<M>, T>
+    constexpr void operator()(M& m, T&& t) const noexcept(noexcept(cpo::unwrap(m) = std::forward<T>(t))) {
+      cpo::unwrap(m) = std::forward<T>(t);
+    }
+
+    template<unwrappable M, typename T>
+      requires (not std::assignable_from<traits::unwrap_raw_t<M>, T>) and
+               std::assignable_from<M&, T>
+    constexpr void operator()(M& m, T&& t) const noexcept(noexcept(m = std::forward<T>(t))) {
+      m = std::forward<T>(t);
+    }
+  };
+
+}
+
+namespace harmony::inline cpo {
+
+  inline constexpr detail::unit_impl unit{};
+}
+
+namespace harmony::inline concepts {
+
+  template<typename M, typename T>
+  concept rewrappable = 
+    unwrappable<M> and
+    requires(M& m, T&& v) {
+      harmony::cpo::unit(m, std::forward<T>(v));
+    };
+
+  template<typename F, typename M>
+  concept monadic = 
+    std::invocable<F, traits::unwrap_t<M>> and
+    rewrappable<M, std::invoke_result_t<F, traits::unwrap_t<M>>>;
+  
+  template<typename U, typename T>
+  concept equivalent_to = std::same_as<std::remove_cvref_t<U>, T>;
+
 }
 
 namespace harmony {
 
   template<unwrappable T>
   class monas {
-    
-    T m_monad;
+
+    static_assert(not (std::is_lvalue_reference_v<T> or std::is_rvalue_reference_v<T>), "T must not be a reference type.");
+
+    T& m_monad;
     
   public:
 
-    template<std::same_as<T> U>
-    constexpr monas(U&& v) : m_monad(std::forward<U>(v)) {}
-    
-    template<std::invocable<traits::content_t<T>> F>
+    constexpr monas(T& monad) noexcept : m_monad(monad) {}
+
+    [[nodiscard]]
+    constexpr auto operator*() noexcept(noexcept(cpo::unwrap(m_monad))) -> traits::unwrap_raw_t<T> {
+      return cpo::unwrap(m_monad);
+    }
+
+    // [[nodiscard]]
+    // constexpr auto operator*() && noexcept(noexcept(std::move(cpo::unwrap(m_monad)))) -> traits::unwrap_t<T> {
+    //   return std::move(cpo::unwrap(m_monad));
+    // }
+
+    [[nodiscard]]
+    constexpr explicit operator bool() const noexcept(noexcept(cpo::validate(m_monad))) requires maybe<T> {
+      return cpo::validate(m_monad);
+    }
+
+    // constexpr operator T() const & noexcept(std::is_nothrow_copy_constructible_v<T>) {
+    //   return m_monad;
+    // }
+
+    constexpr operator T&() {
+      return m_monad;
+    }
+
+    // constexpr operator T() && noexcept(std::is_nothrow_move_constructible_v<T>) {
+    //   return std::move(m_monad);
+    // }
+
+  public:
+
+    template<monadic<T> F>
       requires (not maybe<T>)
-    friend constexpr auto operator|(monas& self, F&& f) -> T {
-      return f(cpo::unwrap(self.m_monad));
+    friend constexpr auto operator|(monas&& self, F&& f) noexcept(std::is_nothrow_invocable_r_v<T, F, traits::unwrap_t<T>>) -> monas<T>&& {
+      cpo::unit(self.m_monad, f(*self));
+      return std::move(self);
     }
     
-    template<std::invocable<traits::content_t<T>> F>
+    template<monadic<T> F>
       requires maybe<T>
-    friend constexpr auto operator|(monas& self, F&& f) -> T {
-      if (cpo::validate(self.m_monad)) {
-        return f(cpo::unwrap(self));
-      } else {
-        return self.m_monad;
+    friend constexpr auto operator|(monas&& self, F&& f) noexcept(std::is_nothrow_invocable_r_v<T, F, traits::unwrap_t<T>>) -> monas<T>&& {
+      if (self) {
+        cpo::unit(self.m_monad, f(*self));
       }
+      return std::move(self);
     }
-    
   };
   
   template<typename T>
-  monas(T&&) -> monas<std::remove_cvref_t<T>>;
+  monas(T&) -> monas<std::remove_cvref_t<T>>;
   
 } // harmony
--- test/harmony_test.cpp
+++ test/harmony_test.cpp
@@ -1,6 +1,144 @@
-#include "harmony.hpp"
 #include "boost/ut.hpp"
+#include "harmony.hpp"
+
+#include <optional>
+#include <vector>
+#include <any>
+
+namespace ut = boost::ut;
 
 int main() {
+  using namespace boost::ut::literals;
+  using namespace boost::ut::operators::terse;
+
+  "concept unwrappable test"_test = [] {
+    ut::expect(harmony::unwrappable<int*>);
+    ut::expect(harmony::unwrappable<std::optional<int>>);
+    ut::expect(harmony::unwrappable<std::vector<int>>);
+  };
+
+  "concept maybe test"_test = [] {
+    ut::expect(harmony::maybe<int *>);
+    ut::expect(harmony::maybe<std::optional<int>>);
+    ut::expect(harmony::maybe<std::vector<int>>);
+  };
+
+  "concept list test"_test = [] {
+    ut::expect(not harmony::list<int *>);
+    ut::expect(not harmony::list<std::optional<int>>);
+    ut::expect(harmony::list<std::vector<int>>);
+  };
+
+  "concept rewrappable test"_test = [] {
+    ut::expect(harmony::rewrappable<int*, int>);
+    ut::expect(harmony::rewrappable<std::optional<int>, int>);
+    ut::expect(harmony::rewrappable<int*, short>);
+    ut::expect(harmony::rewrappable<std::optional<int>, short>);
+    ut::expect(harmony::rewrappable<int*, double>);
+    ut::expect(harmony::rewrappable<std::optional<int>, double>);
+  };
+
+  "cpo unwrap test"_test = [] {
+    {
+      int n = 10;
+      int *p = &n;
+
+      10_i == harmony::unwrap(p);
+
+      n = 20;
+
+      20_i == harmony::unwrap(p);
+    }
+    {
+      std::optional<int> opt = 10;
+
+      10_i == harmony::unwrap(opt);
+
+      opt = 20;
+
+      20_i == harmony::unwrap(opt);
+    }
+  };
+
+  "cpo validate test"_test = [] {
+    {
+      int *p = nullptr;
+
+      ut::expect(not harmony::validate(p));
+
+      int n = 10;
+      p = &n;
+
+      ut::expect(harmony::validate(p));
+    }
+    {
+      std::optional<int> opt = std::nullopt;
+
+      ut::expect(not harmony::validate(opt));
+
+      opt = 10;
+
+      ut::expect(harmony::validate(opt));
+    }
+    {
+      std::vector<int> v{};
+
+      ut::expect(not harmony::validate(v));
+
+      v.push_back(10);
+
+      ut::expect(harmony::validate(v));
+    }
+  };
+
+  "cpo unit test"_test = [] {
+    {
+      int n = 0;
+      int *p = &n;
+
+      harmony::unit(p, 10);
+
+      !ut::expect(harmony::validate(p));
+      10_i == harmony::unwrap(p);
+    }
+    {
+      std::optional<int> opt = 1;
+
+      harmony::unit(opt, 10);
+
+      !ut::expect(harmony::validate(opt));
+      10_i == harmony::unwrap(opt);
+    }
+  };
+
+  "type monas test"_test = [] {
+    {
+      std::optional<int> opt = 10;
+
+      // 暗黙変換可能
+      std::optional<int> result = harmony::monas(opt) | [](int n) { return std::optional<int>{n + n}; } 
+                                                      | [](int n) { return std::optional<int>{n + 100}; };
+
+      !ut::expect(harmony::validate(result));
+      120_i == harmony::unwrap(result);
+      // 状態は起点のオブジェクトに伝搬する
+      ut::expect(opt == result);
+    }
+    {
+      std::optional<int> opt = 10;
   
+      // 途中で失敗する処理のチェーン
+      std::optional<int> result = harmony::monas(opt)
+        | [](int n) { return std::optional<int>{ n + n }; }
+        | [](int n) { return std::optional<int>{ n + 100 };}
+        | [](int)   { return std::nullopt;}
+        | [](int n) { return n*n;};
+
+      ut::expect(not harmony::validate(result));
+      // 状態は起点のオブジェクトに伝搬する
+      ut::expect(opt == result);
+    }
+  };
+
+  //"operation bind test"_test = []{};
 }
